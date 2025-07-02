f4445ac67caed90f37af94735007ca285154b2a1
Denis <denis.gerolini@datacare.it>
2025-07-02 15:11:40 +0200

    Rivoluzionato completamente lo script del Passero post_deply.sh. La logica di base è rimasta la stessa ma è stata inserita la gestione degli errori, redirect su log degli errori, verifica login bpnbat, loggin su stdout del progresso dello script. Potrebbe essere necessario fixare ancora qualcosa ma al momento sembra buono

--- post_deploy.sh
+++ post_deploy.sh
@@ -9,16 +9,15 @@
 ###       ⼭⻲            ###
 #############################
 #############################
-#!/bin/bash
-#
-# In linea di massima lo script è ok, ma bisogna effettuare dei cambiamenti importanti:
-#    1 - gestione degli errori, quando uno script termina e non ha raccolto nulla dobbiamo saperlo 
-#    2 - file di log, gli errori devono essere loggati
-#    3 - raccolta output sotto lo stesso path dello script
-#
+#!/usr/bin/env basenv bash
 
 
-##################################	DENIS
+#Author		: Valerio Passeri
+#Contributor	: Denis Gerolini
+#Version	: 2.0
+
+
+
 #	Path principali NetBackup
 OPENV="/usr/openv/"
 DIR_NBU="${OPENV}netbackup/"
@@ -38,51 +37,109 @@ VXLOGCFG="${BIN}vxlogcfg"
 NBCERTCMD="${BIN}nbcertcmd"
 NBDB_PING="${DB}bin/nbdb_ping"
 BPPS="${BIN}bpps"
+NBSECCMD="${ADMINCMD}nbseccmd"
+NBAUDITREPORT="${ADMINCMD}nbauditreport"
+BPDBJOBS="${ADMINCMD}bpdbjobs"
+BPPLLIST="${ADMINCMD}bppllist"
+BPPLINFO="${ADMINCMD}bpplinfo"
+NBSTLUTIL="${ADMINCMD}nbstlutil"
+NBSTL="${ADMINCMD}nbstl"
+NBDEVQUERY="${ADMINCMD}nbdevquery"
+NBCC="${SUPPORT}NBCC"
+NBSU="${SUPPORT}nbsu"
+NB_DEPLOYMENT_INSIGHTS="${ADMINCMD}netbackup_deployment_insights"
 
-#	Definizione percorso di output e file di log
+#	Definizione percorso di output
 OUTPUT_FOLDER="output_$(date +"%d_%m_%Y")/"
-LOG_FILE="${OUTPUT_FOLDER}post_deploy.log"
-##################################	END DENIS
+POLICY_DETAILS="${OUTPUT_FOLDER}policy_details/"
+NB_DEPLOY_FOLDER="${OUTPUT_FOLDER}nbgather_files"
 
-##################################	DENIS
-# Verifico se è stato effettuato il login con bpnbat
-bpnbat_status=$( ${BPNBAT} -WhoAmI 2>/dev/null)
+# Definizione per il logging
+LOG_ERROR_FILE="${OUTPUT_FOLDER}post_deploy_errors.log"
+
+
+#	Lo script deve essere eseguito come root
+if [ "${EUID}" -ne 0 ]; then
+	echo "Lo script deve essere eseguito con i privilegi di root... esco"
+	exit 1
+fi
+
+stdout_exit_status() {
+	if [ $1 -eq 0 ]; then
+		echo "SUCCESS"
+	else
+		echo "FAILED"
+	fi
+}
+
+# 	Verifico se è stato effettuato il login con bpnbat
+bpnbat_status=$( ${BPNBAT} -WhoAmI 2>>/dev/null)
 if [ $? -eq 0 ]; then
-	echo "Hai già effettuato il login come utente $( echo "${bpnbat_status}" | awk '$1 == "Name:" {print $2}'), continuo..."
+	bpnbat_user=$( echo "${bpnbat_status}" | grep '^Web' -A 3 | awk '$1 == "Name:" {print $2}')
+	echo ""
+	echo "***********************************"
+	echo "***********************************"
+	echo "bpnbat login effettuato come utente ${bpnbat_user}... SUCCESS"
+	echo "***********************************"
+	echo "***********************************"
+	echo ""
+
 else
+
+	echo "***********************************"
+	echo "***********************************"
+	echo 
 	echo "WARNING: non hai effettuato il login bpnbat, alcune funzioni potrebbero non essere disponibili."
+	while true; do
+		read -p "Vuoi comunque proseguire [Y/N]? " continua
+		continua=${continua,,}
+		if [[ "${continua}" == "y" ]]; then
+			echo ""
+			echo "***********************************"
+			echo "***********************************"
+			echo "Continuo con l'esecuzione dello script senza aver effettuato il login bpnbat... WARNING"
+			echo "***********************************"
+			echo "***********************************"
+			echo ""
+			break
+		elif [[ "${continua}" == "n" ]]; then
+			echo "Esco!!"
+			exit 0
+		else
+			echo "Scelta non valida..."
+		fi
+	done
+	echo
+	echo "***********************************"
+	echo "***********************************"
 fi	
 
 # Creo cartella di output
 mkdir -p ${OUTPUT_FOLDER}
 
+#	Svuoto il file di log se già esiste
+cat /dev/null > "${LOG_ERROR_FILE}"
+
 # Copia bp.conf
 echo -n "Copia di /usr/openv/netbackup/bp.conf... "
-cp "${DIR_NBU}bp.conf" "${OUTPUT_FOLDER}"
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+cp "${DIR_NBU}bp.conf" "${OUTPUT_FOLDER}" >/dev/null 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 
 # Copio output nbemmcmd
 echo -n "Copio la lista host NBEMM... "
-nbemmcmd_list_hosts=$(${NBEMMCMD} -listh 2>/dev/null)
+nbemmcmd_list_hosts=$(${NBEMMCMD} -listh 2>>"${LOG_ERROR_FILE}")
 if [ $? -eq 0 ]; then
 	echo "SUCCESS"
 	echo "${nbemmcmd_list_hosts}" > ${OUTPUT_FOLDER}nbemmcmd.txt
 	if echo "${nbemmcmd_list_hosts}" | grep -q '^master'; then
+		
 		machine_type="master"
 	else
 		machine_type="primary"
 	fi
 	echo -n  "Copio le configurazioni del ${machine_type} server... "
-	${NBEMMCMD} -listsettings -machinename $(echo "${nbemmcmd_list_hosts}" | grep -Ei '^primary|^master' | awk '{print $2}') -machinetype ${machine_type} > ${OUTPUT_FOLDER}listemmsettings_audit.txt 2>errori.txt
-	if [ $? -eq 0 ]; then
-		echo "SUCCESS"
-	else
-		echo "FAILED"
-	fi
+	${NBEMMCMD} -listsettings -machinename $(echo "${nbemmcmd_list_hosts}" | grep -Ei '^primary|^master' | awk '{print $2}') -machinetype ${machine_type} > ${OUTPUT_FOLDER}listemmsettings_audit.txt 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 else
 	echo "FAILED"
 fi
@@ -92,143 +149,204 @@ fi
 
 # Media server e drive status
 echo -n "Verifica status media e drive... "
-${VMOPRCMD} > "${OUTPUT_FOLDER}vmoprcmd.txt" 2>/dev/null
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+${VMOPRCMD} > "${OUTPUT_FOLDER}vmoprcmd.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 
 # Verifica spazio filesystem
 echo -n "Verifica spazio filesystem... "
-df -h > "${OUTPUT_FOLDER}df_h.txt" 2>/dev/null
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+df -h > "${OUTPUT_FOLDER}df_h.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 
 # Verifica parametri di logging di default di vxlog
 echo -n "Verifica valori di default di vxlog... "
-${VXLOGCFG} -p nb -o default -l > "${OUTPUT_FOLDER}vxlogcfg.txt" 2>/dev/null
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+${VXLOGCFG} -p nb -o default -l > "${OUTPUT_FOLDER}vxlogcfg.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 
 # Verifica se il DB NB è UP
 echo -n "Verifica lo stato di NBDB... "
-${NBDB_PING} > "${OUTPUT_FOLDER}nbdb_ping.txt" 2>errori.txt
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+${NBDB_PING} > "${OUTPUT_FOLDER}nbdb_ping.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 
 # Recupero le informazioni sui certificati
 echo -n "Recupero informazioni dei certificati a livello di dominio... "
-${NBCERTCMD} -listAllDomainCertificates > "${OUTPUT_FOLDER}certs_domain.txt" 2>errori.txt
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+${NBCERTCMD} -listAllDomainCertificates > "${OUTPUT_FOLDER}certs_domain.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 echo -n "Recupero informazioni dei certificati presenti sul Primary Server... "
-${NBCERTCMD} -listAllcertificates > "${OUTPUT_FOLDER}cert_list.txt" 2>errori.txt
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+${NBCERTCMD} -listAllcertificates > "${OUTPUT_FOLDER}cert_list.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 
 
 # Esecuzione del comando vmrule
 echo -n "Eseguo VMRULE... "
-${VMRULE} -listall >"${OUTPUT_FOLDER}vmrules.txt" 2>errori.txt
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+${VMRULE} -listall >"${OUTPUT_FOLDER}vmrules.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
 
 # Verifica dei servizi attivi
 echo -n "Verifica dei servizi NetBackup attivi... "
-${BPPS} -x > "${OUTPUT_FOLDER}bpps_x.txt" 2>errori.txt
-if [ $? -eq 0 ]; then
-	echo "SUCCESS"
-else
-	echo "FAILED"
-fi
+${BPPS} -x > "${OUTPUT_FOLDER}bpps_x.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+
 
 # Verifico le impostazioni di rete
 echo -n "Verifico le impostazioni di rete... "
 if command -v ip &>/dev/null; then
-	echo "esi"
-	ip 
-elif command -v ifconfig &>/dev/null
-	echo "ifconfig"
+	ip a > "${OUTPUT_FOLDER}ip_all.txt" 2>>"${LOG_ERROR_FILE}"
+	ip r > "${OUTPUT_FOLDER}ip_route.txt" 2>>"${LOG_ERROR_FILE}"
+else command -v ifconfig &>/dev/null
+	ifconfig > "${OUTPUT_FOLDER}ifconfig.txt" 2>>"${LOG_ERROR_FILE}"
+	route > "${OUTPUT_FOLDER}route.txt" 2>>"${LOG_ERROR_FILE}"
+fi
+stdout_exit_status $?
+
+echo -n "Verifica memoria RAM... "
+free -h > "${OUTPUT_FOLDER}free_h.txt"
+stdout_exit_status $?
+
+
+echo -n "Verifica memoria virtuale... "
+vmstat -s > "${OUTPUT_FOLDER}vmstat.txt"
+stdout_exit_status $?
+
+echo -n "Copio il file /etc/passwd... "
+if cp /etc/passwd "${OUTPUT_FOLDER}" 2>>"${LOG_ERROR_FILE}"; then
+	echo "SUCCESS"
 else
+	echo "FAILED"
+fi
+
+echo -n "Copio il file /etc/group... "
+if cp /etc/group "${OUTPUT_FOLDER}" 2>>"${LOG_ERROR_FILE}"; then
+	echo "SUCCESS"
+else
+	echo "FAILED"
+fi
+
+
+echo -n "Copio il file /etc/hosts... "
+if cp /etc/hosts "${OUTPUT_FOLDER}" 2>>"${LOG_ERROR_FILE}"; then
+	echo "SUCCESS"
+else
+	echo "FAILED"
+fi
+
+echo -n "Copio il file /etc/resolv.conf... "
+if cp /etc/resolv.conf "${OUTPUT_FOLDER}" 2>>"${LOG_ERROR_FILE}"; then
+	echo "SUCCESS"
+else
+	echo "FAILED"
+fi
+
+echo -n "Copio il file /etc/fstab... "
+if cp /etc/fstab "${OUTPUT_FOLDER}" 2>>"${LOG_ERROR_FILE}"; then
+	echo "SUCCESS"
+else
+	echo "FAILED"
+fi
+
+echo -n "Eseguo il NetBackup Security Configuration service utility (nbseccmd)... "
+${NBSECCMD} -getsecurityconfig -auditretentionperiod > "${OUTPUT_FOLDER}auditreport_retention.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Estrapolo il report di audit (nbauditreport) dal 1 gennaio 2000... "
+${NBAUDITREPORT} -sdate 01/01/2000 -notruncate > "${OUTPUT_FOLDER}auditreport_retention.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Estrapolo il report di audit dettagliato (nbauditreport) dal 1 gennaio 2000... "
+${NBAUDITREPORT} -sdate 01/01/2000 -notruncate -fmt DETAIL > "${OUTPUT_FOLDER}auditreport_details.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Interrogo il database dei job... "
+${BPDBJOBS} -summary > "${OUTPUT_FOLDER}summary_jobs.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Scarico la lista delle policy... "
+${BPPLLIST} > "${OUTPUT_FOLDER}lista_policy.txt" 2>>"${LOG_ERROR_FILE}"
+if [ $? -eq 0 ]; then
+	echo "SUCCESS"
+	mkdir -p "${POLICY_DETAILS}"
+	echo "Scarico il dettaglio delle policy... "
+	detail_failed=0
+	for policy in $(cat ${OUTPUT_FOLDER}lista_policy.txt); do
+		echo -e -n "\tDownload dettaglio policy ${policy}... "
+		${BPPLINFO} ${policy} -L > "${POLICY_DETAILS}${policy}.info" 2>>"${LOG_ERROR_FILE}"
+		if [ $? -eq 0 ]; then
+			echo "SUCCESS"
+		else
+			echo "FAILED"
+			$(( detail_failed + 1 ))
+		fi
+	done
+else
+	echo "FAILED"
+fi
+
+echo -n "Report SLP... "
+${NBSTLUTIL} report > "${OUTPUT_FOLDER}slp_backlog.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Download lista SLP... "
+${NBSTL} -L > "${OUTPUT_FOLDER}slp_list.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Download lista versioni SLP... "
+${NBSTL} -L -all_versions > "${OUTPUT_FOLDER}slp_list_all_versions.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Download lista Disk Pool presenti... "
+${NBDEVQUERY} -listdp -U > "${OUTPUT_FOLDER}dpool_list.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Download lista Storage Server presenti... "
+${NBDEVQUERY} -liststs -L > "${OUTPUT_FOLDER}storage_pool.txt" 2>>"${LOG_ERROR_FILE}"
+stdout_exit_status $?
+
+echo -n "Eseguo NBCC, attendi... "
+${NBCC} -nocleanup >/dev/null 2>>"${LOG_ERROR_FILE}"
+if [ $? -eq 0 ]; then
+	echo "SUCCESS"
+	mv output "${OUTPUT_FOLDER}nbcc_output" 2>"${LOG_ERROR_FILE}"
+else
+	echo "FAILED"
+fi
+
+echo -n "Eseguo nbsu, attendi... "
+nbsu_stdout=$( ${NBSU} 2>>"${LOG_ERROR_FILE}")
+if [ $? -eq 0 ]; then
+	echo "SUCCESS"
+	mv $( echo "${nbsu_stdout}" | tail | grep '^Final NBSU output' | awk '{print $NF}' ) "${OUTPUT_FOLDER}"
+	echo "${nbsu_stdout}" > "${OUTPUT_FOLDER}nbsu_log.txt"
+else
+	echo "FAILED"
+fi
+
+
+echo -e "\n***********************************"
+echo "Devo eseguire netbackup_deployment_insights, inserisci alcuni dati e attendi il completamento..."
+${NB_DEPLOYMENT_INSIGHTS} --gather --report --capacity --hoursago 8766 2>>"${LOG_ERROR_FILE}"
+if [ $? -eq 0 ]; then
+	echo "nb_deployment_insight terminato con successo"
+	report_path="/usr/openv/var/global/reports"
+	last_report="$(ls -td "${report_path}"/*/ | head -n 1 )"
+	tar czf "${OUTPUT_FOLDER}nb_deployment_insights_report.tgz" ${last_report}
+else
+	echo "Errore durante l'esecuzione di nb_deployment_insight"
+fi
+echo -e "***********************************\n"
+
+tarout="${OUTPUT_FOLDER%/}"
+tar czf "${tarout}.tgz" "${OUTPUT_FOLDER}" 2>> "${LOG_ERROR_FILE}"
+if [ $? -ne 0 ]; then
+	echo "ERROR, non sono riuscito a creare l'archivio dei report presenti in ${OUTPUT_FOLDER}"
+	exit 1
+fi
+
+echo  -e "\n###############################################################################################################################"
+echo  "####### Scritp terminato, è stato creato il file ${OUTPUT_FOLDER}.tgz nella directory corrente contenente tutti i report"
+echo  -e "###############################################################################################################################\n"
+echo ""
+if [ -s "${LOG_ERROR_FILE}" ]; then
+	echo "Verifica eventuali errori nel file ${LOG_ERROR_FILE}"
 fi
 
 exit 0
-
-# Non è detto che il comando ip sia presente
-ip a > ${DIR_OUT}/ip.txt
-ip r > ${DIR_OUT}/route.txt
-
-# Questo comando non va bene, o meglio potrebbe anche andare bene ma non è detto che utilizzi Network Manager
-nmcli > ${DIR_OUT}/nmcli.txt
-date > ${DIR_OUT}/memory.txt ; free -h >> ${DIR_OUT}/memory.txt
-date > ${DIR_OUT}/vmstat.txt ; vmstat -s >> ${DIR_OUT}/vmstat.txt
-cat /etc/passwd | grep -i nb > ${DIR_OUT}/nb_users.txt
-cat /etc/group | grep -i nb > ${DIR_OUT}/nb_groups.txt
-cp /etc/hosts ${DIR_OUT}/hosts
-cp /etc/resolv.conf ${DIR_OUT}/resolv.conf
-cp /etc/fstab ${DIR_OUT}/fstab
-nbseccmd -getsecurityconfig -auditretentionperiod > ${DIR_OUT}/auditreport_retention.txt
-nbauditreport -sdate 01/01/2000 -notruncate > ${DIR_OUT}/auditreport_summary.txt
-nbauditreport -sdate 01/01/2000 -notruncate -fmt DETAIL > ${DIR_OUT}/auditreport_details.txt
-bpdbjobs -summary > ${DIR_OUT}/summary_jobs.txt
-bppllist > ${DIR_OUT}/lista_policy.txt
-for i in ${DIR_OUT}/lista_policy.txt; do bpplinfo $i -L >> ${DIR_OUT}/dettaglio_policy.txt; echo "------------------" >> ${DIR_OUT}/dettaglio_policy.txt; done
-nbstlutil report > ${DIR_OUT}/backlog_SLP.txt
-nbstl -L > ${DIR_OUT}/lista_SLP.txt
-nbstl -L -all_versions > ${DIR_OUT}/lista_SLP_ALLVERSIONS.txt
-nbdevquery -listdp -U > ${DIR_OUT}/diskpool_list.txt
-nbdevquery -liststs -L > ${DIR_OUT}/storage_server_list.txt
-
-
-# NBCC e nbsu, non c'è modo di utilizzare una funzione nativa per l'output? Ad esempio redirigendo direttamente e nativamente un file nella cartella desiderata
-echo "raccolgo NBCC"
-NBCC -nocleanup > ${DIR_OUT}/nbcc_log
-cp -r $(tail -2 ${DIR_OUT}/nbcc_log | grep out | awk '{print $1}') ${DIR_OUT}/
-
-echo "raccolgo nbsu"
-nbsu &> ${DIR_OUT}/nbsu_log.txt
-cp -r $(cat ${DIR_OUT}/nbsu_log.txt | grep Final | awk '{print $6}') ${DIR_OUT}/
-
-echo "##################################################"
-echo "Ora lancio nbdeployutil con info sull'ultimo anno, sono richieste le informazioni per continuare..."
-data=$(date +"%Y%m%d_%H")
-nbdeployutil --gather --report --capacity --hoursago 8766
-
-wait
-for i in `ls /usr/openv/var/global/reports/${data}*/report-capacity-*.xls`;
-do
-cp $i "${DIR_OUT}"
-done
-
-echo "File xls del nbdeployutil copiato in ${DIR_OUT}"
-
-echo "##################################################"
-echo "##################################################"
-echo "ABBIAMO RACCCOLTO TUTTI I DATI, ora creo il tar..."
-echo "##################################################"
-echo "##################################################"
-tar cvf $DIR_ZIP $DIR_OUT
-echo "##################################################"
-echo "##################################################"
-echo "PUOI PORTARE VIA IL TAR $DIR_ZIP"
-echo "##################################################"
-echo "##################################################"
